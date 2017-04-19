009caeeac5e16bd942f56d179f63202731b6e164
wh-Coder <461891475@qq.com>
2017-04-19 17:27:32 +0800

    分支

--- service.js
+++ service.js
@@ -1,39 +1,33 @@
-/**
- * Created by Administrator on 2017-04-19.
- */
 const http = require('http');
 const fs = require('fs');
 const querystring = require('querystring');
 const urlLib = require('url');
 
 var server = http.createServer((req, res) => {
-    // GET
-    let obj = urlLib.parse(req.url, true);
-    let url = obj.pathname;
-    const GET = obj.query;
-
-    // POST
-    let str = '';
+    var str = '';
     req.on('data', (data) => {
         str += data;
     });
     req.on('end', () => {
-        const POST = querystring.parse(str);
-        console.log(url, GET, POST);
-    })
+        var obj = urlLib.parse(req.url, true);
 
-    // 文件请求
-    let file_name = './www' + url;
-    fs.readFile(file_name,(err,data) => {
-        if(err){
-            res.write('404');
-        }else{
-            res.write(data);
-        }
-        res.end();
+        const url = obj.pathname;
+        const GET = obj.query;
+        const POST = querystring.parse(str);
+
+        var file_name = './www' + url;
+        fs.readFile(file_name, (err, data) => {
+            if (err) {
+                res.write('404');
+            } else {
+                res.write(data)
+            }
+            res.end()
+        });
+        console.log(url, GET, POST);
     })
 });
 
 server.listen(7777);
 
-console.log('连接成功');
+console.log('http://localhost:7777');
